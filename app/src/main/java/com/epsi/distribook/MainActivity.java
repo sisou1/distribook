b721f193c81751627ebde9554ab21619a2071a19
furkan <kocaslanf49@gmail.com>
2023-10-05 09:11:24 +0200

    new

--- app/src/main/java/com/epsi/distribook/MainActivity.java
+++ app/src/main/java/com/epsi/distribook/MainActivity.java
@@ -5,37 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageButton account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        account = findViewById(R.id.logo_account);
+        account.setOnClickListener(v -> {
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_item, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.home2) {
-            // Ouvrez la nouvelle activité lorsque l'élément de menu est sélectionné
-            Intent intent = new Intent(this, ConnexionActivity.class);
+            Intent intent = new Intent(MainActivity.this, ConnexionActivity.class);
             startActivity(intent);
-            return true;
+        });
+
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
-}
